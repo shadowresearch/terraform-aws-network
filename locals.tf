6853e12437a7041a58907ae0d0eca7b7f65bed5f
Dalyan Parker <dalyan@shadow.inc>
2025-02-25 21:24:47 -0500

    feat: add aws network (#3)
    * feat: add aws network
    
    * fix: remove security groups

--- locals.tf
+++ locals.tf
@@ -1,9 +1,11 @@
 locals {
   subnets = {
-    "database"    = 6,
-    "elasticache" = 6,
-    "intra"       = 5,
-    "private"     = 3,
-    "public"      = 5,
+    # "database"    = 6,
+    # "elasticache" = 6,
+    # "intra"       = 5,
+    # "private"     = 3,
+    # "public"      = 5,
+    "private" = 2,
+    "public"  = 2,
   }
 }
