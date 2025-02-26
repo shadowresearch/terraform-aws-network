0951112693de9f5e3985c23c4c03e41fb038173d
Dalyan Parker <dalyan@shadow.inc>
2025-02-25 21:19:15 -0500

    feat: add aws network

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
 
-  azs                    = var.azs
-  cidr                   = var.cidr
-  database_subnets       = [for az in var.azs : module.subnets.network_cidr_blocks["database-${az}"]]
-  elasticache_subnets    = [for az in var.azs : module.subnets.network_cidr_blocks["elasticache-${az}"]]
-  enable_nat_gateway     = true
-  intra_subnets          = [for az in var.azs : module.subnets.network_cidr_blocks["intra-${az}"]]
+  azs  = var.azs
+  cidr = var.cidr
+  # database_subnets       = [for az in var.azs : module.subnets.network_cidr_blocks["database-${az}"]]
+  # elasticache_subnets    = [for az in var.azs : module.subnets.network_cidr_blocks["elasticache-${az}"]]
+  # enable_nat_gateway     = true
+  # intra_subnets          = [for az in var.azs : module.subnets.network_cidr_blocks["intra-${az}"]]
   name                   = var.name
   one_nat_gateway_per_az = false
   private_subnets        = [for az in var.azs : module.subnets.network_cidr_blocks["private-${az}"]]
@@ -36,13 +36,13 @@ module "vpc" {
     }
   ]
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
+  # private_subnet_tags = {
+  #   "kubernetes.io/role/internal-elb" = 1
+  # }
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+  # public_subnet_tags = {
+  #   "kubernetes.io/role/elb" = 1
+  # }
 
   tags = {
     Network   = var.name
